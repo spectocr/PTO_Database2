ee5d855789ff0f1f523e22f703deaae2054fbf8a
spectocr <highstakes22@gmail.com>
2022-01-18 11:42:37 -0500

    making the changes

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
+import {ReactComponent as ReactLogo} from '../images/cenlar_logo.svg';
+// import { ReactComponent as Logo } from './cenlar_logo.svg'
+
+
+const Header = () => {
+  const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  };
+  return (
+    <header className="bg-secondary mb-4 py-2 flex-row align-center">
+      <div className="container flex-row justify-space-between-lg justify-center align-center">
+        <Link to="/">
+        <ReactLogo />
+        </Link>
+        <h1>PTO Database</h1>
+        <nav className="text-center">
+          
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">My Profile</Link>
+              <a href="/" onClick={logout}>
+                Logout
+      </a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              {/* <Link to="/signup">Signup</Link> */}
+            </>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
